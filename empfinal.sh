adb8abea795fcb904ea3a8bd1a16670d07eac5a3
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-09 17:17:41 +0530

    check present or not

---
+++ empfinal.sh
@@ -0,0 +1,16 @@
+!/bin/bash -x                                                                  
+ present=$((RANDOM %2))
+ Rateperhour=50
+ if [ $present==1 ]
+  then
+               
+                 hourpresent=8
+  else  [ $present==0 ]
+                
+                 echo "absent"
+ 
+                 
+
+
+
+fi

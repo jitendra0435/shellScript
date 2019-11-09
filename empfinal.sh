a942981e9f0b91b5b99c58d65dcebdd029b425c7
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-09 17:44:09 +0530

    addparttimefeature

--- empfinal.sh
+++ empfinal.sh
@@ -1,16 +1,21 @@
-!/bin/bash -x                                                                  
- present=$((RANDOM %2))
+/bash -x                                                                  
+ present=$((RANDOM %3))
  Rateperhour=50
  if [ $present==1 ]
   then
-               
                  echo "present"
-  else  [ $present==0 ]
-                
+                 hourpresent=8
+  elif [ $present==0 ]
+  then
+                 hourpresent=0
                  echo "absent"
- 
-                 
+  else
+                 hourpresent=4
+                 echo "part time worker"
+
+ fi
+    salary=$(($hourpresent*$Rateperhour))
+    echo $salary
 
 
 
-fi

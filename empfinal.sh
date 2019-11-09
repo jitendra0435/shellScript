cb584e384fc6c8d29dae5d35a77f4a158ab87bcb
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-09 18:17:18 +0530

    change the name empCheck

--- empfinal.sh
+++ empfinal.sh
@@ -1,16 +1,18 @@
-!/bin/bash -x                                                                  
- present=$((RANDOM %2))
+#!/bin/bash -x                                                                  
+ randomCheck=$((RANDOM %3))
  Rateperhour=50
  if [ $present==1 ]
   then
-               
                  echo "present"
-  else  [ $present==0 ]
-                
+                 hourpresent=8
+  elif [ $present==0 ]
+  then
+                 hourpresent=0
                  echo "absent"
- 
-                 
+  else
+                 hourpresent=4
+                 echo "part time worker"
 
-
-
-fi
+ fi
+    salary=$(($hourpresent*$Rateperhour))
+    echo $salary

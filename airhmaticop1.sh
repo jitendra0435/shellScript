1bca177f025ffc28305fcdc991f5021d88da697d
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-11 18:52:38 +0530

    Print the result using Array and loop

--- airhmaticop1.sh
+++ airhmaticop1.sh
@@ -1,19 +1,17 @@
 
 #!/bin/bash -x
-read -p "enter value for a": a
-read -p "enter value for b": b
-read -p "enter value for c": c
+	read -p "enter value for a": a
+	read -p "enter value for b": b
+	read -p "enter value for c": c
 
-declare -A  operations 
-operations[result1]=$(($a+$b*$c))
-operations[result2]=$(($a*$b+$c))
-operations[result3]=$(($c+$a/$b))
-operations[result4]=$(($a%$b+$c))
-echo "output in Dictionary format"
-echo "operation->1" ${operations[result1]}
-echo "operation->2" ${operations[result2]}
-echo "operation->3" ${operations[result3]}
-echo "operation->4" ${operations[result4]}
+	declare -A  operations 
+	operations[result1]=$(($a+$b*$c))
+	operations[result2]=$(($a*$b+$c))
+	operations[result3]=$(($c+$a/$b))		
+	operations[result4]=$(($a%$b+$c))
 
-
-
+	for ((i=0;i<5;i++))
+ 	 do
+	 array=${operations[@]}
+	 done
+ 	 echo $array

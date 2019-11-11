924646e2ee90a6a80594d34ec0120e91fcf1be87
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-11 10:25:59 +0530

    change the random name to empCheck

--- employeeSwitchcase.sh
+++ employeeSwitchcase.sh
@@ -1,21 +1,19 @@
- 
+
 #!/bin/bash -x
          isFulltime=1
          isParttime=2
 	 random=$((RANDOM %3))
 	 Rateperhour=50
- 
- 	 case $random in 
-		$isFulltime)             
+
+ 	 case $random in
+		$isFulltime )
                  hourpresent=8;;
 
      	        * )
 	        hourpresent=0;;
-             $isParttime )
-                 
+
+               $isParttime )
                  hourpresent=4;;
-                
-  
      esac
     salaryofEmployee=$(($hourpresent*$Rateperhour))
 

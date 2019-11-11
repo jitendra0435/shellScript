90eeb4bfe4d432b859345e2571fd2bf448b82d5b
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-11 12:36:53 +0530

    add array feature

--- empfinal.sh
+++ empfinal.sh
@@ -1,26 +1,33 @@
 
-#!/bin/bash -x
+#!/bin/bash -x 
+ Rateperhour=40
  function myfunction ()
 {
  echo "hi"
- empCheck=$((RANDOM%3))                                                                 
- Rateperhour=50
- if [ $empCheck==1 ]
+ empCheck=$((RANDOM%3))
+echo $empCheck                                     
+if [ $empCheck == 1 ]
   then
                  echo "present"
                  hourpresent=8
-  elif [ $empCheck==0 ]
-  then
-                 hourpresent=0
-                 echo "absent"
-  else
+  elif [ $empCheck == 2 ]
+   then
                  hourpresent=4
+                 echo "parttime"
+  else
+                 hourpresent=0
                  echo "part time worker"
 
- fi
+fi
     salary=$(($hourpresent*$Rateperhour))
     echo $salary
 
 }
 
-result="$(myfunction)"
+for((count=1;count<=20;count++))
+ do
+   
+ result=$(myfunction)
+ echo ${result[@]}
+ done
+printf "\n"

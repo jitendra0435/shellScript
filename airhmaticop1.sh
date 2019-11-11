1c444b58286ade9de049b59d00e804e09edb6652
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-11 17:54:00 +0530

    display result into dictionary

--- airhmaticop1.sh
+++ airhmaticop1.sh
@@ -4,14 +4,15 @@ read -p "enter value for a": a
 read -p "enter value for b": b
 read -p "enter value for c": c
 
-result1=$(($a+$b*$c))
-result2=$(($a*$b+$c))
-result3=$(($c+$a/$b))
-result4=$(($a%$b+$c))
-echo $result1
-echo $result2
-echo $result3
-echo $result4
+declare -A  operations 
+operations[result1]=$(($a+$b*$c))
+operations[result2]=$(($a*$b+$c))
+operations[result3]=$(($c+$a/$b))
+operations[result4]=$(($a%$b+$c))
+echo "operation->1" ${operations[result1]}
+echo "operation->2" ${operations[result2]}
+echo "operation->3" ${operations[result3]}
+echo "operation->4" ${operations[result4]}
 
 
 

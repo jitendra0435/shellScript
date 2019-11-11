a67369c00b9132e75dafb24d4bc5af64fa05d16b
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-11 17:58:15 +0530

    display the output in dictionary format

--- airhmaticop1.sh
+++ airhmaticop1.sh
@@ -9,6 +9,7 @@ operations[result1]=$(($a+$b*$c))
 operations[result2]=$(($a*$b+$c))
 operations[result3]=$(($c+$a/$b))
 operations[result4]=$(($a%$b+$c))
+echo "output in Dictionary format"
 echo "operation->1" ${operations[result1]}
 echo "operation->2" ${operations[result2]}
 echo "operation->3" ${operations[result3]}

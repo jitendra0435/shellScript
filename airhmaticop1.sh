d70db14f8e3216ebdead6c12e47d5e96b63e393a
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-11 17:35:09 +0530

    add more airthmatic operations

--- airhmaticop1.sh
+++ airhmaticop1.sh
@@ -7,9 +7,11 @@ read -p "enter value for c": c
 result1=$(($a+$b*$c))
 result2=$(($a*$b+$c))
 result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
 echo $result1
 echo $result2
 echo $result3
+echo $result4
 
 
 

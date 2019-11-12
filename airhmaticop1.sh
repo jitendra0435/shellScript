00d07a6b31ce16c1ec4a0071fc990e4e7f2a3157
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-12 10:05:00 +0530

    Print results in Ascending order

--- airhmaticop1.sh
+++ airhmaticop1.sh
@@ -9,9 +9,6 @@
 	operations[result2]=$(($a*$b+$c))
 	operations[result3]=$(($c+$a/$b))		
 	operations[result4]=$(($a%$b+$c))
-
-	for ((i=0;i<5;i++))
- 	 do
-	 array=${operations[@]}
-	 done
- 	 echo $array
+        
+	 echo "Results in Ascending order" $(printf "%s\n" ${operations[@]} | sort -n )
+         
